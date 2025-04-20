7f496908407195f5a23ef5ce79c7ae25bd720ccb
Darkhero766 <nightowlclub72@gmail.com>
2025-04-20 12:53:04 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,4 +1,3 @@
-import random
 import tensorflow as tf
 from tensorflow.keras import layers, models
 import numpy as np
@@ -7,17 +6,16 @@ import numpy as np
 model = models.Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
     layers.MaxPooling2D((2, 2)),
-  
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.Flatten(),
-  
+    
     layers.Dense(64, activation='relu'),
-    layers.Dense(3, activation='softmax')
+    layers.Dense(6, activation='softmax')
 ])
 
-countries = ['India', 'Japan', 'Brazil']
+countries = ['India', 'Japan', 'Brazil', 'France', 'Germany', 'USA']
 )
 num_samples = 100
 X_train = []
@@ -27,17 +25,18 @@ y_train = []
 for _ in range(num_samples):
     img = np.ones((128, 128, 3))
     img[:42, :] = [1.0, 0.5, 0]  # Orange
+    
     img[43:85, :] = [1.0, 1.0, 1.0]  # White
     img[86:, :] = [0, 0.8, 0]  # Green
     img += np.random.normal(0, 0.1, img.shape)  
     img = np.clip(img, 0, 1)
-  
+    
     X_train.append(img)
     y_train.append(0)
 
 # Japan flag
 for _ in range(num_samples):
-    img = np.ones((128, 128, 3))  # White background
+    img = np.ones((128, 128, 3))  
     X_train.append(img)
     y_train.append(1)  
 
@@ -47,6 +46,40 @@ for _ in range(num_samples):
     img[:, :] = [0, 0.8, 0]  # Green
     X_train.append(img)
     y_train.append(2)
+
+# France flag
+for _ in range(num_samples):
+    img = np.ones((128, 128, 3))
+    img[:, :42] = [0, 0, 1]  # Blue
+    img[:, 43:85] = [1, 1, 1]  # White
+    img[:, 86:] = [1, 0, 0]  # Red
+    img += np.random.normal(0, 0.1, img.shape)
+    img = np.clip(img, 0, 1)
+    X_train.append(img)
+    y_train.append(3)
+
+# Germany flag
+for _ in range(num_samples):
+    img = np.ones((128, 128, 3))
+    img[:42, :] = [0, 0, 0]  # Black
+    img[43:85, :] = [1, 0, 0]  # Red
+    img[86:, :] = [1, 1, 0]  # Yellow
+    img += np.random.normal(0, 0.1, img.shape)
+    img = np.clip(img, 0, 1)
+    
+    X_train.append(img)
+    y_train.append(4)
+
+# USA flag
+for _ in range(num_samples):
+    img = np.ones((128, 128, 3))
+    img[::2, :] = [1, 0, 0]  # Red and white stripes
+    img[:42, :42] = [0, 0, 1]  # Blue canton
+    img += np.random.normal(0, 0.1, img.shape)
+    img = np.clip(img, 0, 1)
+    X_train.append(img)
+    
+    y_train.append(5)
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
